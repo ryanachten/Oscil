da4234cab608727e9ee9b093599fb7c56596b72e
Ryan Achten <ryanachten@gmail.com>
2017-04-15 17:01:47 +1200

    Refactor visualisation function into various visualisation functions, add select element for vis modes

--- js/oscil.js
+++ js/oscil.js
@@ -1,20 +1,18 @@
+/*
+Resources:
+	mdn/voice-change-o-matic
+	https://github.com/mdn/voice-change-o-matic/blob/gh-pages/scripts/app.js
+*/
+
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var analyser = audioCtx.createAnalyser();
 
-//Microphone access
-navigator.getUserMedia (
-	{
-		audio: true
-	},
-	function(stream) {
-		source = audioCtx.createMediaStreamSource(stream);
-		source.connect(analyser);
-	  },
 
-	function(err) {
-		console.log('The following gUM error occured: ' + err);
-	}
-);
+//Smoothing / calibration settings - these should all be sliders
+// analyser.minDecibels = -90;
+// analyser.maxDecibels = -10;
+// analyser.smoothingTimeConstant = 0.85;
+
 
 //Canvas Setup
 var bgColor = 'rgb(237, 230, 224)';
@@ -27,7 +25,47 @@ var canvasCtx = canvas.getContext('2d'); //wonder what this does?
 canvasCtx.fillStyle = bgColor;
 canvasCtx.fillRect(0,0, canvWidth, canvHeight);
 
-function barGraph(active){
+var visualisationMode = document.querySelector('#visual-select');
+var drawVisual;
+
+
+//Microphone access
+navigator.getUserMedia (
+	{
+		audio: true
+	},
+	function(stream) {
+		source = audioCtx.createMediaStreamSource(stream);
+		source.connect(analyser);
+
+		visualise(visualisationMode.value);
+	  },
+
+	function(err) {
+		console.log('The following gUM error occured: ' + err);
+	}
+);
+
+function visualise(visMode){
+	console.log(visMode);
+	if(visMode === 'BarGraph'){
+		barGraph(); 
+	}
+	else if(visMode === 'WaveForm'){
+		waveForm();
+	}
+	else if(visMode === 'Off'){
+		visOff();
+	}
+}
+
+function visOff(){
+	canvasCtx.clearRect(0,0,canvWidth, canvHeight);
+	canvasCtx.fillStyle = bgColor;
+	canvasCtx.fillRect(0,0,canvWidth, canvHeight);
+}
+
+function barGraph(){
 	analyser.fftSize = 256;
     var bufferLength = analyser.frequencyBinCount;
     console.log(bufferLength);
@@ -66,72 +104,48 @@ function barGraph(active){
 
 
 //Waveform/oscilloscope
-function waveForm(active){
+function waveForm(){
 
-	console.log(active);
+	analyser.fftSize = 2048; //defines Fast Fourier Transform rate 
+	var bufferLength = analyser.frequencyBinCount; // length of freqBinCount is half the fft
+	var dataArray = new Uint8Array(bufferLength); //defines how many datapoints collecting for that fft size
 
-	// if(active === true){
-		analyser.fftSize = 2048; //defines Fast Fourier Transform rate 
-		var bufferLength = analyser.frequencyBinCount; // length of freqBinCount is half the fft
-		var dataArray = new Uint8Array(bufferLength); //defines how many datapoints collecting for that fft size
+	canvasCtx.clearRect(0,0,canvWidth,canvHeight); //reset canvas for new vis
 
-		canvasCtx.clearRect(0,0,canvWidth,canvHeight); //reset canvas for new vis
+	function draw(){
+		drawVisual = requestAnimationFrame(draw); //this keeps looping the drawing function once it has started
+		analyser.getByteTimeDomainData(dataArray); //retrieve the data and copy it into our array
+		canvasCtx.fillStyle = bgColor;
+		canvasCtx.fillRect(0,0, canvWidth, canvHeight);
 
-		var drawVisual;
+		//line width and color
+		canvasCtx.lineWidth = 1;
+			canvasCtx.strokeStyle = 'rgb(0,0,0)';
+			canvasCtx.beginPath();
 
-		function draw(){
-			drawVisual = requestAnimationFrame(draw); //this keeps looping the drawing function once it has started
-			analyser.getByteTimeDomainData(dataArray); //retrieve the data and copy it into our array
-			canvasCtx.fillStyle = bgColor;
-			canvasCtx.fillRect(0,0, canvWidth, canvHeight);
+		//width of ea. segment = canv.w / arraylength
+		var sliceWidth = canvWidth * 1.0 / bufferLength;
+			var x = 0; //position to move to to draw ea. line segment
 
-			//line width and color
-			canvasCtx.lineWidth = 1;
-				canvasCtx.strokeStyle = 'rgb(0,0,0)';
-				canvasCtx.beginPath();
+		for(var i=0; i <bufferLength; i++){
+			var v = dataArray[i] / 128.0; //128.0 height based on the data point value form the array
+			var y = v * canvHeight/2;
 
-			//width of ea. segment = canv.w / arraylength
-			var sliceWidth = canvWidth * 1.0 / bufferLength;
-				var x = 0; //position to move to to draw ea. line segment
-
-			for(var i=0; i <bufferLength; i++){
-				var v = dataArray[i] / 128.0; //128.0 height based on the data point value form the array
-				var y = v * canvHeight/2;
-
-				if(i===0){
-					canvasCtx.moveTo(x,y); // moving the line across to the place where the next wave segment should be drawn
-				}else{
-					canvasCtx.lineTo(x,y);
-				}
-				x += sliceWidth;
+			if(i===0){
+				canvasCtx.moveTo(x,y); // moving the line across to the place where the next wave segment should be drawn
+			}else{
+				canvasCtx.lineTo(x,y);
 			}
-
-			canvasCtx.lineTo(canvWidth, canvWidth/2);
-				canvasCtx.stroke();
+			x += sliceWidth;
 		}
-		if(active === true){
-			draw();
-		}	
-		else{
-			console.log('clear');
-			// drawVisual = requestAnimationFrame(draw);
-			cancelAnimationFrame(drawVisual);
-			canvasCtx.clearRect(0,0,canvWidth,canvHeight);
-		}	
+
+		canvasCtx.lineTo(canvWidth, canvWidth/2);
+			canvasCtx.stroke();
+	}
+		draw();
 }
 
-// var waveToggle = false;
-// $('#waveform-button').click(function(){
-// 	if(waveToggle){
-// 		waveForm(false);
-// 		console.log("waveForm(false);");
-// 		waveToggle = false;
-// 	}else{
-// 		waveForm(true);
-// 		console.log("waveForm(true);");
-// 		waveToggle = true;
-// 	}
-
-// });
-// waveForm(true);
-barGraph(true);
+visualisationMode.onchange = function(){
+	window.cancelAnimationFrame(drawVisual);
+	visualise(visualisationMode.value);
+}
