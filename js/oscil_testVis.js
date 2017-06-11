644bc89fb92670ee23049412be12b11d92c19c73
Ryan Achten <ryanachten@gmail.com>
2017-06-11 18:58:59 +1200

    Rename addLetter function as addWord, add addLetter functionality

--- js/oscil_testVis.js
+++ js/oscil_testVis.js
@@ -9,33 +9,63 @@ function tests(dataArray, bufferLength){
 
 	function typeDither(){
 
-		// var canvas2 = document.createElement('canvas');
-		// canvas2.width = 20; canvas2.height = 20;
-		// var canv2Width = canvas2.width; var canv2Height = canvas2.height;
-		// var canvas2Ctx = canvas2.getContext('2d');
-		// document.getElementById('container').appendChild(canvas2);
-		// canvas2.style.display = 'none';
-
 		var textSample = 'Ryan Achten';
 		textSample = textSample.toUpperCase();
 
 		var maxFontSize = canvHeight/4;
 		var leading = 100;
 		var xPos, yPos, endOfPage, startPos;
-		var fontSize = 100;
-
+		var fontSize;
 
 		// create the off-screen canvas
 		var canvasPattern = document.createElement("canvas");
 		canvasPattern.id = "canvasPattern";
-		var patternWidth = 20;
-		var patternHeight = 20;
+		var patternWidth = patternHeight = 20;
 		canvasPattern.width = patternWidth;
 		canvasPattern.height = patternHeight;
 		var contextPattern = canvasPattern.getContext("2d");
-		yPos = fontSize;
+
+		var textMode = 'letter'; //or 'letter'
+		var decorationMode = 'dash'; //or 'pattern'
 		
+		var letterIndex = 0;
+
 		function init(){
+
+			if(textMode === 'word'){
+				fontSize = 100;
+				startAnimating(5);
+			}
+			else{
+				startAnimating(2);
+			}
+		}
+		init();
+
+
+		function draw(){
+
+			if(textMode === 'word'){
+				addWord();
+			}
+			else{
+				//cycle setup
+				canvasCtx.clearRect(0,0, canvWidth, canvHeight);
+				canvasCtx.fillStyle = bgColor;
+				canvasCtx.fillRect(0,0, canvWidth, canvHeight);
+				canvasCtx.fillStyle = 'black';
+
+				addLetter(letterIndex);
+				letterIndex++;
+				if(letterIndex >= textSample.length) letterIndex = 0;
+			}
+
+		}
+
+
+		function addWord(){
+
+			//cycle setup
 			canvasCtx.clearRect(0,0, canvWidth, canvHeight);
 			canvasCtx.fillStyle = bgColor;
 			canvasCtx.fillRect(0,0, canvWidth, canvHeight);
@@ -47,92 +77,106 @@ function tests(dataArray, bufferLength){
 
 			xPos = startPos;
 			yPos = canvHeight/2;
-			endOfPage = false;
-		}
-		init();
 
-
-		function addLetter(){
-
-			init();
 
 			for(var i = 0; i < textSample.length; i++){
+
 				xPos += fontSize;
-				// yPos += fontSize;
-				// if((yPos) > canvHeight){
-				// 	yPos = 0;
-				// }
+
 				canvasCtx.font = fontSize + "px Arial";
 
-				function dashStrokePatterns(){
-					var dashLength = Math.floor((Math.random()*10)+1);
-					var dashGapLength = Math.floor((Math.random()*10)+1);
-					canvasCtx.setLineDash([dashLength,dashGapLength]);
-					canvasCtx.lineWidth = Math.floor((Math.random()*10)+1);
-					var joinRand = Math.floor((Math.random()*3)+1)
-					switch(joinRand){
-						case 1 :
-							canvasCtx.joinStyle = 'bevel';
-							break;
-						case 2 :
-							canvasCtx.joinStyle = 'round';
-							break;
-						case 3 :
-							canvasCtx.joinStyle = 'mitre';
-							canvasCtx.mitreLength = Math.floor((Math.random()*10)+1);
-							break;
-						default : 
-							canvasCtx.joinStyle = 'mitre';
-							canvasCtx.mitreLength = Math.floor((Math.random()*10)+1);
-					}
-				}
-				dashStrokePatterns();
-
-				function drawPatternStroke(){
-
-					contextPattern.clearRect(0,0,patternWidth,patternHeight);
-
-					// Dot Pattern
-					canvasCtx.lineWidth = 10;
-					contextPattern.beginPath();
-					var radius = Math.floor(Math.random()*10+0);
-					contextPattern.arc(patternWidth/2, patternHeight/2, radius, 0, Math.PI*2);
-					var colorRand = Math.floor(Math.random()*2);
-					if(colorRand === 1){
-						canvasCtx.fillStyle = 'black';
-						contextPattern.fill();
-					}else{
-						canvasCtx.strokeStyle = 'black';
-						contextPattern.stroke();
-					}
-					
-					//Cross Pattern
-					canvasCtx.lineWidth = 10;
-					canvasCtx.fillStyle = 'black';
-					contextPattern.lineWidth = Math.random()*5;
-					var margin = 4;
-					contextPattern.beginPath();
-					contextPattern.moveTo(0+margin,patternHeight/2);
-					contextPattern.lineTo(patternWidth-margin, patternHeight/2);
-					contextPattern.moveTo(patternWidth/2,0+margin);
-					contextPattern.lineTo(patternWidth/2,patternHeight-margin);
-					contextPattern.strokeStyle = 'black';
-					contextPattern.stroke();
-
-
-					var pattern = canvasCtx.createPattern(canvasPattern,"repeat");
-					canvasCtx.strokeStyle = pattern;
-
-					canvasCtx.fillStyle = pattern;
-					canvasCtx.fillRect(40, 40, 100, 100);
-					canvasCtx.fill();
+				if(decorationMode === 'dash'){
+					dashStrokePatterns();
+				}else{
+					drawPatternStroke();
 				}
 
-				// setTimeout(drawPatternStroke,100);
-				// drawPatternStroke();
 				canvasCtx.strokeText(textSample[i], xPos + (Math.random()*(fontSize*2)), yPos + (Math.random()*(fontSize*2)));
 
+				contextPattern.clearRect(0,0,patternWidth,patternHeight);
 			}
+		}
+
+
+		function addLetter(i){
+
+			fontSize = (Math.random()*(canvWidth/4))+100;
+			xPos = canvWidth/2 - fontSize/2;
+			yPos = canvHeight/2 + fontSize/4;
+
+			canvasCtx.font = fontSize + "px Arial";
+
+			if(decorationMode === 'dash'){
+				dashStrokePatterns();
+			}else{
+				drawPatternStroke();
+			}
+
+			canvasCtx.strokeText(textSample[i], xPos + (Math.random()*(fontSize/4)),
+									yPos + (Math.random()*(fontSize/4)));
+
+			contextPattern.clearRect(0,0,patternWidth,patternHeight);
+		}
+
+
+		function dashStrokePatterns(){
+			var dashLength = Math.floor((Math.random()*10)+1);
+			var dashGapLength = Math.floor((Math.random()*10)+1);
+			canvasCtx.setLineDash([dashLength,dashGapLength]);
+			canvasCtx.lineWidth = Math.floor((Math.random()*10)+1);
+			var joinRand = Math.floor((Math.random()*3)+1);
+			switch(joinRand){
+				case 1 :
+					canvasCtx.joinStyle = 'bevel';
+					break;
+				case 2 :
+					canvasCtx.joinStyle = 'round';
+					break;
+				case 3 :
+					canvasCtx.joinStyle = 'mitre';
+					canvasCtx.mitreLength = Math.floor((Math.random()*10)+1);
+					break;
+				default : 
+					canvasCtx.joinStyle = 'mitre';
+					canvasCtx.mitreLength = Math.floor((Math.random()*10)+1);
+			}
+		}
+				
+		function drawPatternStroke(){
+
+			// Dot Pattern
+			canvasCtx.lineWidth = 10;
+			contextPattern.beginPath();
+			var radius = Math.floor(Math.random()*10+0);
+			contextPattern.arc(patternWidth/2, patternHeight/2, radius, 0, Math.PI*2);
+			var colorRand = Math.floor(Math.random()*2);
+			if(colorRand === 1){
+				canvasCtx.fillStyle = 'black';
+				contextPattern.fill();
+			}else{
+				canvasCtx.strokeStyle = 'black';
+				contextPattern.stroke();
+			}
+			
+			//Cross Pattern
+			canvasCtx.lineWidth = 10;
+			canvasCtx.fillStyle = 'black';
+			contextPattern.lineWidth = Math.random()*5;
+			var margin = 4;
+			contextPattern.beginPath();
+			contextPattern.moveTo(0+margin,patternHeight/2);
+			contextPattern.lineTo(patternWidth-margin, patternHeight/2);
+			contextPattern.moveTo(patternWidth/2,0+margin);
+			contextPattern.lineTo(patternWidth/2,patternHeight-margin);
+			contextPattern.strokeStyle = 'black';
+			contextPattern.stroke();
+
+			var pattern = canvasCtx.createPattern(canvasPattern,"repeat");
+			canvasCtx.strokeStyle = pattern;
+
+			canvasCtx.fillStyle = pattern;
+			canvasCtx.fillRect(40, 40, 100, 100);
+			canvasCtx.fill();
 		}
 		
 		var stop = false;
@@ -160,10 +204,9 @@ function tests(dataArray, bufferLength){
 			if(elapsed > fpsInterval){
 				then = now - (elapsed % fpsInterval);
 
-				addLetter();
+				draw();
 			}
 		}
-		startAnimating(5);
 	}
-	// typeDither();
+	typeDither();
 }
