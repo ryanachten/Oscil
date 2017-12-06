5273561dbb29a1a99af26588c3b6bfdc07811f21
Ryan Achten <ryanachten@gmail.com>
2017-12-06 17:23:26 +1300

    Add first 3d visual experiment

--- js/threedVisuals.js
+++ js/threedVisuals.js
@@ -1,4 +1,4 @@
-function threedTest(){
+function threedTest(dataArray, bufferLength){
 
     $('#visualiser').hide();
 
@@ -15,36 +15,109 @@ function threedTest(){
     var camera = new THREE.PerspectiveCamera(35,
       window.innerWidth / window.innerHeight, 0.1, 3000);
 
+      camera.position.set(0, 0, 1000);
+
+      document.addEventListener("keydown", function(event) {
+        if (event.keyCode == '38') {
+          //Up
+          camera.position.z += 50;
+        }
+        else if (event.keyCode == '40') {
+          //Down
+          camera.position.z -= 50;
+        }
+      });
+
+
     // Scene setup
     var scene = new THREE.Scene();
 
     // Light setup
       // light( colour, strength)
     var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    ambientLight.position.set(0, 0, 0);
     scene.add(ambientLight);
 
     var pointLight = new THREE.PointLight(0xffffff, 0.5);
+    pointLight.position.set(0, 1000, 1000);
     scene.add(pointLight);
 
-    // Geometry
-      // cube(x,y,z)
-    var geometry = new THREE.CubeGeometry(100, 100, 100);
-    var material = new THREE.MeshLambertMaterial( {color: 0x47d0ef} );
-    var mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(0, 0, -1000);
+    var reactParticles = [];
+    function createReactParticle(){
+      var reactParticle = new THREE.Object3D();
+      reactParticle.name = 'reactParticle';
 
-    scene.add(mesh);
+      // Particle nucleus
+      var nucleusGeo = new THREE.IcosahedronGeometry(50, 1);
+      var nucleusMat = new THREE.MeshLambertMaterial(
+        { color: 0xef4773} );
+        // { color: 0xef4773, transparent: true, opacity: 0} );
+      var nucleusMesh = new THREE.Mesh(nucleusGeo, nucleusMat);
+      reactParticle.add(nucleusMesh);
+      nucleusMesh.name = 'nucleusMesh';
+
+      // Particle outer dust
+      var sphereParticles = new THREE.Object3D();
+      sphereParticles.name = 'sphereParticles';
+
+      for (var i = 0; i < nucleusGeo.vertices.length; i++){
+        var tempVert = new THREE.Vector3(nucleusGeo.vertices[i].x, nucleusGeo.vertices[i].y, nucleusGeo.vertices[i].z);
+
+        var tempSphereGeo = new THREE.SphereGeometry(5, 20, 20)
+        var tempSphereMat = new THREE.MeshLambertMaterial(
+          { color: 0x47d0ef });
+        var tempSphereMesh = new THREE.Mesh(tempSphereGeo, tempSphereMat);
+        tempSphereMesh.position.set(tempVert.x, tempVert.y, tempVert.z);
+        sphereParticles.add(tempSphereMesh);
+      }
+
+      reactParticle.add(sphereParticles);
+      return reactParticle;
+    }
+
+    var reactParticles = [];
+
+
+    var particleGridGeo = new THREE.DodecahedronGeometry(500, 0);
+    for (var i = 0; i < particleGridGeo.vertices.length; i++) {
+      var tempPart = createReactParticle();
+      var tempPos = particleGridGeo.vertices[i];
+      tempPart.position.set(tempPos.x, tempPos.y, tempPos.z);
+      reactParticles.push(tempPart);
+      scene.add(tempPart);
+    }
+
 
     // Render Loop
     requestAnimationFrame(render);
 
     function render(){
-      mesh.rotation.x += 0.01;
-      mesh.rotation.y += 0.01;
+
+      analyser.getByteFrequencyData(dataArray);
+      var da = dataArray[0]/255 +0.01;
+
+      function updateReactParticle(particle){
+        var nucleus = particle.children[0];
+        var sphereParticles = particle.children[1];
+
+        for (var i = 0; i < sphereParticles.children.length; i++) {
+          sphereParticles.children[i].scale.x = sphereParticles.children[i].scale.y = sphereParticles.children[i].scale.z = 1-da;
+        }
+        sphereParticles.rotation.x -= 0.01;
+        sphereParticles.rotation.y -= 0.01;
+        sphereParticles.rotation.z -= 0.01;
+
+        nucleus.scale.x = nucleus.scale.y = nucleus.scale.z = da;
+        nucleus.rotation.x += 0.01;
+        nucleus.rotation.y += 0.01;
+        nucleus.rotation.z += 0.01;
+      }
+      for (var i = 0; i < reactParticles.length; i++) {
+          updateReactParticle(reactParticles[i]);
+      }
 
       renderer.render(scene, camera);
 
       requestAnimationFrame(render);
     }
-
 }
