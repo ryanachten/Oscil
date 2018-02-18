e04ebc78932d48f7e0ea612ccd99dafe2759596a
Ryan Achten <ryanachten@gmail.com>
2018-02-18 14:54:07 +1300

    Add barGraph visual

--- src/components/visuals/BarGraph.js
+++ src/components/visuals/BarGraph.js
@@ -34,54 +34,49 @@ class Waveform extends React.Component{
       this.dataArray = dataArray;
       this.draw();
     }).catch( (reason) => {
-        // Do something
-        console.log(reason);
-      }
-    );
+      // Do something
+      console.log(reason);
+    }
+  );
+}
+
+draw(){
+  this.frameId = requestAnimationFrame(this.draw);
+
+  const canvasCtx = this.canvasCtx;
+  const {canvWidth, canvHeight} = this.state;
+  const bufferLength = this.bufferLength;
+  const dataArray = this.dataArray;
+
+  this.analyser.getByteTimeDomainData(this.dataArray);
+  canvasCtx.fillStyle = 'white';
+  canvasCtx.fillRect(0, 0, canvWidth, canvHeight);
+
+  const barWidth = (canvWidth / bufferLength) * 2;
+  let barHeight;
+  let x = 0;
+
+  for(let i = 0; i < bufferLength; i++) {
+    barHeight = dataArray[i]*3;
+
+    const y = canvHeight/2-barHeight/2;
+
+    canvasCtx.fillStyle = 'hsl('+ barHeight +',50%,70%)';
+    canvasCtx.fillRect(x,y,barWidth,barHeight);
+
+    x += barWidth;
   }
+}
 
-  draw(){
-    this.frameId = requestAnimationFrame(this.draw);
+componentWillUnmount(){
+  cancelAnimationFrame(this.frameId);
+}
 
-    const canvasCtx = this.canvasCtx;
-    const {canvWidth, canvHeight} = this.state;
-
-    this.analyser.getByteTimeDomainData(this.dataArray);
-    canvasCtx.fillStyle = 'white';
-    canvasCtx.fillRect(0,0, canvWidth, canvHeight);
-    canvasCtx.lineWidth = 2;
-
-    canvasCtx.beginPath();
-    const sliceWidth = canvWidth * 1.0 / this.bufferLength;
-		let x = 0;
-
-		for(let i = 0; i < this.bufferLength; i++){
-			const v = this.dataArray[i] / 128.0;
-			canvasCtx.strokeStyle = 'hsl('+ this.dataArray[i]*5 +',80%,70%)';
-			const y = v * canvHeight/2;
-
-			if(i===0){
-				canvasCtx.moveTo(x,y);
-			}else{
-				canvasCtx.lineTo(x,y);
-			}
-			x += sliceWidth;
-		}
-
-		canvasCtx.lineTo(canvWidth, canvWidth/2);
-			canvasCtx.stroke();
-
-  }
-
-  componentWillUnmount(){
-    cancelAnimationFrame(this.frameId);
-  }
-
-  render(){
-    return(
-      <canvas ref={(canvas) => {this.canvas = canvas}}></canvas>
-    );
-  }
+render(){
+  return(
+    <canvas ref={(canvas) => {this.canvas = canvas}}></canvas>
+  );
+}
 }
 
 export default Waveform;
