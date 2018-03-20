13719af7c07d12bb8581946c03bebf0459ca1517
Ryan Achten <ryanachten@gmail.com>
2018-03-21 12:11:23 +1300

    Add vis thumb grid to landing

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import visuals from '../store/visuals';
 
 class HomePage extends React.Component{
 
-
   render(){
     return(
-      <div>
-        Home page
+      <div className="thumbnail--grid">
+        { Object.keys(visuals).map((visual) => {
+          const currentVis = visuals[visual];
+          return(
+            <Link className="thumbnail--item" key={visual} to={`/${visual}`}>
+              <div className="thumbnail--background"
+                style={{backgroundImage: `url(${currentVis.thumbImg})`}}>
+                <p className="thumbnail--label">{visual}</p>
+              </div>
+            </Link>
+          )
+        })}
       </div>
     )
   }
