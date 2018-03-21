c8940608783ae2291e38ec25e9aada9ec77a495f
Ryan Achten <ryanachten@gmail.com>
2018-03-22 10:26:57 +1300

    Move bundled files into dist dir, Setup dev depedencies

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env) => {
   return {
     entry: "./src/app.js",
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: "bundle.js"
     },
     module: {
@@ -48,7 +48,8 @@ module.exports = (env) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   }
 };
