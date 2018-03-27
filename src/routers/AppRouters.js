fa96424bcd80819c8a6cd4d825e8555a02819518
Ryan Achten <ryanachten@gmail.com>
2018-03-27 13:16:28 +1300

    Add 404 catch to approuter when user adds visual url which doesn't exist or is deleted

--- src/routers/AppRouters.js
+++ src/routers/AppRouters.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import allVisualData from '../store/visuals';
 
 import Header from '../components/Header';
 import HomePage from '../components/HomePage';
 import NotFoundPage from '../components/NotFoundPage';
 import VisualPage from '../components/VisualPage';
 
-const AppRouter = (props) => (
-  <BrowserRouter>
-    <div>
-    <Header />
-      <Switch>
-        <Route path="/" component={HomePage} exact={true} />
-          <Route path="/:id"
-            component={({ match }) => {
-              return <VisualPage pathId={match.params.id} />
-            }} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
+const AppRouter = (props) => {
+
+  return(
+    <BrowserRouter>
+      <div>
+      <Header />
+        <Switch>
+          <Route path="/" component={HomePage} exact={true} />
+            <Route path="/:id"
+              component={({ match }) => {
+                if (allVisualData[match.params.id]) {
+                  // If current url exists in store/visuals return vis page
+                  return <VisualPage pathId={match.params.id} />
+                }
+                else{
+                  // If current url doesn't exist in store/visuals return 404
+                  return <NotFoundPage />
+                }
+              }} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
+};
 
 export default AppRouter;
