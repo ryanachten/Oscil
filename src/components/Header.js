e609e29d374e0d37cdc987aeeda77deead6e5627
Ryan Achten <ryanachten@gmail.com>
2018-03-27 13:03:18 +1300

    Add init style and image tests to front page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Header = () => (
-  <header>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-    <NavLink to="/portfolio" activeClassName="is-active">Portfolio</NavLink>
-    <NavLink to="/contact" activeClassName="is-active">Contact</NavLink>
-  </header>
-);
+const Header = () => {
+    return(
+      <header>
+        <NavLink
+          id="topnav--oscillogo"
+          to="/" activeClassName="is-active" exact={true}>
+        </NavLink>
+        <NavLink to="/" activeClassName="is-active" exact={true}>oscil</NavLink>
+      </header>
+    )
+};
 
 export default Header;
