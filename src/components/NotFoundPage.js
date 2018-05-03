11a8431263b87f35bfc4ec90093fafaeab2da3de
Ryan Achten <ryanachten@gmail.com>
2018-05-03 19:48:30 +1200

    Add 404 page and styles

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Header from './Header';
+
 const NotFoundPage = () => (
-  <div>
-    404 page component - <Link to="/">Go home</Link>
+  <div className="notfound">
+    <Header currentPage="404" />
+    <div className="notfound__container">
+      <img className="notfound__logo" src="/img/oscil_logo_light.svg"></img>
+      <h1>Sorry!<br />
+      The visual or page you’re looking for can’t be found…</h1>
+      <h2>Check out <Link to="/">home</Link> to find something else!</h2>
+    </div>
   </div>
 );
 
