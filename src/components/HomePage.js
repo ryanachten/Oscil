10406fe4280c184c2bbdb0b873bf80bcc6c062d7
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2022-12-09 15:56:35 -0800

    fix: hardcode default route (hack)

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import $ from 'jquery';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import $ from "jquery";
 
-import Header from './Header';
-import visuals from '../store/visuals';
-import {unsetVisual} from '../actions/visual';
+import Header from "./Header";
+import visuals from "../store/visuals";
+import { unsetVisual } from "../actions/visual";
 
-class HomePage extends React.Component{
-
-  componentWillMount(){
+class HomePage extends React.Component {
+  componentWillMount() {
     this.props.dispatch(unsetVisual());
     let bgImgs = {};
     Object.keys(visuals).map((visual) => {
       const currentImg = visuals[visual].thumbImg;
       const img = new Image();
-      img.src = `/img/thumbs/${currentImg}`;
+      img.src = `./img/thumbs/${currentImg}`;
       img.onload = () => {
         bgImgs[visual] = img;
         if (Object.keys(bgImgs).length === Object.keys(visuals).length) {
@@ -25,34 +24,46 @@ class HomePage extends React.Component{
     });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className="HomePage">
         <Header currentPage="home" />
         <div className="thumbnail--grid">
-          { Object.keys(visuals).map((visual) => {
+          {Object.keys(visuals).map((visual) => {
             const currentVis = visuals[visual];
-            return(
-              <Link className={`thumbnail--item ${visual}`} key={visual} to={`/${visual}`}>
-                <div className="thumbnail--background"
-                  style={{backgroundImage:
-                    `url('/img/thumbs/${currentVis.thumbImg}')`
-                  }}>
+            return (
+              <Link
+                className={`thumbnail--item ${visual}`}
+                key={visual}
+                to={`/${visual}`}
+              >
+                <div
+                  className="thumbnail--background"
+                  style={{
+                    backgroundImage: `url('./img/thumbs/${currentVis.thumbImg}')`,
+                  }}
+                >
                   <div className="thumbnail--info">
-                    <h3 className="thumbnail--title">{visual[0].toUpperCase() + visual.slice(1)}</h3>
+                    <h3 className="thumbnail--title">
+                      {visual[0].toUpperCase() + visual.slice(1)}
+                    </h3>
                     <p>{currentVis.description}</p>
                     <div>
-                      <div className={`thumbnail--type ${currentVis.type}`}></div>
-                      <div className={`thumbnail--renderer ${currentVis.renderer}`}></div>
+                      <div
+                        className={`thumbnail--type ${currentVis.type}`}
+                      ></div>
+                      <div
+                        className={`thumbnail--renderer ${currentVis.renderer}`}
+                      ></div>
                     </div>
                   </div>
                 </div>
               </Link>
-            )
+            );
           })}
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/routers/AppRouters.js
+++ src/routers/AppRouters.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import allVisualData from '../store/visuals';
+import React from "react";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import allVisualData from "../store/visuals";
 
-import HomePage from '../components/HomePage';
-import NotFoundPage from '../components/NotFoundPage';
-import VisualPage from '../components/VisualPage';
+import HomePage from "../components/HomePage";
+import NotFoundPage from "../components/NotFoundPage";
+import VisualPage from "../components/VisualPage";
 
 const AppRouter = (props) => {
-
-  return(
-    <BrowserRouter>
+  return (
+    <BrowserRouter basename="Oscil">
       <div>
         <Switch>
           <Route path="/" component={HomePage} exact={true} />
-            <Route path="/:id"
-              component={({ match }) => {
-                if (allVisualData[match.params.id]) {
-                  // If current url exists in store/visuals return vis page
-                  return <VisualPage pathId={match.params.id} />
-                }
-                else{
-                  // If current url doesn't exist in store/visuals return 404
-                  return <NotFoundPage />
-                }
-              }} />
+          <Route
+            path="/:id"
+            component={({ match }) => {
+              if (allVisualData[match.params.id]) {
+                // If current url exists in store/visuals return vis page
+                return <VisualPage pathId={match.params.id} />;
+              } else {
+                // If current url doesn't exist in store/visuals return 404
+                return <NotFoundPage />;
+              }
+            }}
+          />
           <Route component={NotFoundPage} />
         </Switch>
       </div>
     </BrowserRouter>
-  )
+  );
 };
 
 export default AppRouter;
